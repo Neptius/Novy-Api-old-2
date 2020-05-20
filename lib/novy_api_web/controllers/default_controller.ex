f00e2161f3370a87713bd425abf251f27aaba0f8
Neptius <aymeric.legros@gmail.com>
2020-05-20 02:39:03 +0200

    change

--- lib/novy_api_web/controllers/default_controller.ex
+++ lib/novy_api_web/controllers/default_controller.ex
@@ -2,6 +2,6 @@ defmodule NovyApiWeb.DefaultController do
   use NovyApiWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "api-data.json", data: %{"message" => "Hi, Tenno!"})
+    render(conn, "api-data.json", data: %{"message" => "Hi, Tenno!!"})
   end
 end
